c84dfac8e6a8fb04910d0233edde07b3436dc1fc
Conor Gould <conor.gould@griffithuni.edu.au>
2020-01-27 21:16:43 +1000

    updated tf record convertor

--- to_tf_records.py
+++ to_tf_records.py
@@ -7,30 +7,68 @@ flags = tf.app.flags
 flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
 FLAGS = flags.FLAGS
 
+def mot16_to_tf(coordinates, width, height):
+    """
+    Converts MOT16 bounding box coordinates into TF_Record-friendly data
 
-def create_tf_example(example):
+    Params:
+    coordinates (list of lists): each list containing bounding box data
+        (bb_left, bb_top, bb_width, bb_height)
+    width (int): width of image
+    height (int): height of image
+
+    Returns:
+    xmins (list of floats): min x coordinates for each bounding box
+    xmaxs (list of floats): max x coords
+    ymins (list of floats): min y coords
+    ymaxs (list of floats): max y coords
+    """
+    xmins = []
+    xmaxs = []
+    ymins = []
+    ymaxs = []
+    for box in coordinates:
+        xmin = float(box[0]) / width
+        xmax = (float(box[0]) + float(box[2])) / width
+        ymin = float(box[1]) / height
+        ymax = (float(box[1]) + float(box[3])) / height
+        xmins.append(xmin)
+        xmaxs.append(xmax)
+        ymin.append(ymin)
+        ymaxs.append(ymax)
+
+    return xmins, xmaxs, ymins, ymaxs
+
+
+def create_tf_example(example, path, data_list):
   # TODO(user): Populate the following variables from your example.
-  height = None # Image height
-  width = None # Image width
-  filename = None # Filename of the image. Empty if image is not from file
-  encoded_image_data = None # Encoded image bytes
-  image_format = None # b'jpeg' or b'png'
+  height = 1080 # Image height
+  width = 1920 # Image width
+  filename = example + '.jpg' # Filename of the image. Empty if image is not from file
+  #encoded_image_data = None
+  img_path = path + example + '.jpg'
+  with tf.gfile.GFile(img_path, 'rb') as fid:
+    encoded_jpg = fid.read() # Encoded image bytes
+  image_format = b'jpeg' # b'jpeg' or b'png'
+  # Coordinates is list of lists, each containing bounding box coordinates
+  coordinates = data_list
+  xmins, xmaxs, ymins, ymaxs = mot16_to_tf(coordinates, width, height)
+  #xmins = [] # List of normalized left x coordinates in bounding box (1 per box)
+  #xmaxs = [] # List of normalized right x coordinates in bounding box
+             # (1 per box)
+  #ymins = [] # List of normalized top y coordinates in bounding box (1 per box)
+  #ymaxs = [] # List of normalized bottom y coordinates in bounding box
+             # (1 per box)
 
-  xmins = [] # List of normalized left x coordinates in bounding box (1 per box)
-  xmaxs = [] # List of normalized right x coordinates in bounding box
-             # (1 per box)
-  ymins = [] # List of normalized top y coordinates in bounding box (1 per box)
-  ymaxs = [] # List of normalized bottom y coordinates in bounding box
-             # (1 per box)
-  classes_text = [] # List of string class name of bounding box (1 per box)
-  classes = [] # List of integer class id of bounding box (1 per box)
+  classes_text = ['Pedestrian'] * (len(coordinates)) # List of string class name of bounding box (1 per box)
+  classes = [1] * (len(coordinates)) # List of integer class id of bounding box (1 per box)
 
   tf_example = tf.train.Example(features=tf.train.Features(feature={
       'image/height': dataset_util.int64_feature(height),
       'image/width': dataset_util.int64_feature(width),
       'image/filename': dataset_util.bytes_feature(filename),
       'image/source_id': dataset_util.bytes_feature(filename),
-      'image/encoded': dataset_util.bytes_feature(encoded_image_data),
+      'image/encoded': dataset_util.bytes_feature(encoded_jpg),
       'image/format': dataset_util.bytes_feature(image_format),
       'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
       'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
@@ -44,11 +82,18 @@ def create_tf_example(example):
 
 def main(_):
   writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
-
   # TODO(user): Write code to read in your dataset to examples variable
 
+  # TODO: Dict with filenames as key and list of lists as value
+  data_dict = {}
+
+  train_path = "C:/Users/Conor/Documents/Uni/2019tri3/img/final_proj/data/train/"
+  test_path = "C:/Users/Conor/Documents/Uni/2019tri3/img/final_proj/data/test/"
+
   for example in examples:
-    tf_example = create_tf_example(example)
+    # Get value for key example
+    data_list = data_dict[example]
+    tf_example = create_tf_example(example, train_path, data_list)
     writer.write(tf_example.SerializeToString())
 
   writer.close()

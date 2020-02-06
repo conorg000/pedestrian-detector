7039b560720e32e05f3925ba5fc975b4ea7a0fe7
Conor Gould <conor.gould@griffithuni.edu.au>
2020-02-07 09:46:50 +1000

    updated script

--- scripts/make_labels.py
+++ scripts/make_labels.py
@@ -34,25 +34,26 @@ def clean(train_path, test_path, train_dirs, test_dirs, out_path):
 
     # Start with training directory
     # For each subdirectory in directory, add subdirectory name to line
+    """
     for dir in train_dirs:
         with open("{}{}/det/det.txt".format(train_path, dir), "r") as file:
             lines = file.readlines()
         newlines = []
         for i, line in enumerate(lines):
-            first = len(line.split(",")[0])
-            zeros = (6 - first)
-            line = dir + "_" + (zeros * "0") + line
-            lines[i] = line
+            # Rename image name
+            first = (line.split(",")[0])
+            zeros = (6 - len(first))
+            # Adjust coordinates for smaller images
+            coords = line.split(',')[2:6]
+            coords[0] = float(coords[0])/1920*600
+            coords[1] = float(coords[1])/1080*337
+            coords[2] = float(coords[2])/1920*600
+            coords[3] = float(coords[3])/1080*337
+            new_line = "{}_{}{},{},{},{},{}".format(dir,(zeros*"0"),first,coords[0],coords[1],coords[2],coords[3])
+            lines[i] = new_line + '\n'
 
         with open("{}{}/det/det.txt".format(train_path, dir), "w+") as file:
             file.writelines(lines)
-
-        # For each image in img1 subdirectory, rename images
-        img_path = train_path + dir + "/img1/"
-        images = [f for f in listdir(img_path) if isfile(join(img_path, f))]
-        #print(images)
-        for img in images:
-            rename((img_path + img), (img_path + dir + "_" + img))
 
     # Same with test directory
     for dir in test_dirs:
@@ -60,35 +61,72 @@ def clean(train_path, test_path, train_dirs, test_dirs, out_path):
             lines = file.readlines()
         newlines = []
         for i, line in enumerate(lines):
-            first = len(line.split(",")[0])
-            zeros = (6 - first)
-            line = dir + "_" + (zeros * "0") + line
-            lines[i] = line
+            # Rename image name
+            first = (line.split(",")[0])
+            zeros = (6 - len(first))
+            # Adjust coordinates for smaller images
+            coords = line.split(',')[2:6]
+            coords[0] = float(coords[0])/1920*600
+            coords[1] = float(coords[1])/1080*337
+            coords[2] = float(coords[2])/1920*600
+            coords[3] = float(coords[3])/1080*337
+            new_line = "{}_{}{},{},{},{},{}".format(dir,(zeros*"0"),first,coords[0],coords[1],coords[2],coords[3])
+            lines[i] = new_line '\n'
 
         with open("{}{}/det/det.txt".format(test_path, dir), "w+") as file:
             file.writelines(lines)
-
-        # For each image in img1 subdirectory, rename images
-        img_path = test_path + dir + "/img1/"
-        images = [f for f in listdir(img_path) if isfile(join(img_path, f))]
-        #print(images)
-        for img in images:
-            rename((img_path + img), (img_path + dir + "_" + img))
-
+    """
     # Move train files and create train detection labels
     with open(out_path + "train_det.txt", "w") as outfile:
         for dir in train_dirs:
             # Add detection labels to train_det.txt
+            with open("{}{}/det/det.txt".format(train_path, dir), "r") as file:
+                lines = file.readlines()
+            for i, line in enumerate(lines):
+                # Rename image name
+                first = (line.split(",")[0])
+                zeros = (6 - len(first))
+                # Adjust coordinates for smaller images
+                coords = line.split(',')[2:6]
+                coords[0] = float(coords[0])/1920*600
+                coords[1] = float(coords[1])/1080*337
+                coords[2] = float(coords[2])/1920*600
+                coords[3] = float(coords[3])/1080*337
+                new_line = "{}_{}{},{},{},{},{}".format(dir,(zeros*"0"),first,coords[0],coords[1],coords[2],coords[3])
+                #lines[i] = new_line + '\n'
+                outfile.write(new_line + '\n')
+            """
+            outfile.write(lines)
+                with open("{}{}/det/det.txt".format(train_path, dir), "w+") as file:
+                    file.writelines(lines)
             with open("{}{}/det/det.txt".format(train_path, dir), "r") as infile:
                 for line in infile:
                     outfile.write(line)
+            """
 
     # Move test files and create test detection labels
     with open(out_path + "test_det.txt", "w") as outfile:
         for dir in test_dirs:
+            with open("{}{}/det/det.txt".format(test_path, dir), "r") as file:
+                lines = file.readlines()
+            for i, line in enumerate(lines):
+                # Rename image name
+                first = (line.split(",")[0])
+                zeros = (6 - len(first))
+                # Adjust coordinates for smaller images
+                coords = line.split(',')[2:6]
+                coords[0] = float(coords[0])/1920*600
+                coords[1] = float(coords[1])/1080*337
+                coords[2] = float(coords[2])/1920*600
+                coords[3] = float(coords[3])/1080*337
+                new_line = "{}_{}{},{},{},{},{}".format(dir,(zeros*"0"),first,coords[0],coords[1],coords[2],coords[3])
+                #lines[i] = new_line + '\n'
+                outfile.write(new_line + '\n')
             # Add detection labels to test_det.txt
+            """
             with open("{}{}/det/det.txt".format(test_path, dir), "r") as infile:
                 for line in infile:
                     outfile.write(line)
+            """
 
 clean(train_path, test_path, train_dirs, test_dirs, out_path)
